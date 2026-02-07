b36d40045034a74d7f27c7b3282ebbf0086b12f5
lou437104 <louiseguest73@gmail.com>
2026-02-07 18:22:19 +0000

    cool code

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //turn pages when click next or prev button 
-const pageTurnBtn = document.querySelectorAll('.nextprev-btn back');
-
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn.back');
 
 pageTurnBtn.forEach((el, index) => {
     el.onclick = () => {
         const pageTurnId = el.getAttribute('data-page');
         const pageTurn = document.getElementById(pageTurnId);
 
-        if(pageTurn.classList.contains('turn')){
-            pageTurn.classList.remove('turn');
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn'); // Remove 'turn' class to turn back
+            setTimeout(()=> {})
+        } else {
+            pageTurn.classList.add('turn'); // Add 'turn' class to turn forward
         }
-        else{
-            pageTurn.classList.add('turn');
-        }
-    }
-})
+    };
+});
